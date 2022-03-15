6302e4f2c79c042734a116ca7932f91701bd7b8f
Ludvig Lilleberg <ludvig.lilleberg@visma.com>
2022-03-15 17:27:40 +0100

    Separated CRA app into client folder, added express server to help handle backend Stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton =({ price }) => {
   const priceForStripe = price * 100
   const publishableKey = 'pk_test_51JP8X6BIdvLSIymZYpv8eZjCQNwHyWgaMFQUvwZUghYrzQluDnv0s3of7f3pEnoXyJFXmcbW4DDE5Za0KxNVCEmU00TDwRU0JL'
 
   const onToken = token => {
-    alert('Payment Successful')
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error))
+      alert(
+        'There was an issue with your payment. Please make sure you use the provided credit card.'
+      )
+    })
   }
 
   return (

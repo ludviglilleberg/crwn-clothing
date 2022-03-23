c90e93018edf1cda894c5645619ca2019363fd53
Ludvig Lilleberg <ludvig.lilleberg@visma.com>
2022-03-23 07:31:17 +0100

    Updated app to be PWA compliant

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require ('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -15,6 +16,7 @@ const port = process.env.PORT || 5001;
 app.use(compression())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
@@ -26,6 +28,12 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port ' + port);
+})
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, '..', 'build', 'service-worker.js')
+  )
 })
 
 app.post('/payment', (req, res) => {
